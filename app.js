4d200ba0d62608847c2462fce21662f1ac9eaa5b
Kiril Komitski <k.komitski94@gmail.com>
2021-10-27 16:30:30 +0100

    Template literals in ES6+

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
-let text = '     Peter Jordanson';
-let textLowerCase = text.toLowerCase();
-let textUpperCase = text.toUpperCase();
+const name = 'john';
+const age = 25;
+const sentence = "Hey it's me " + name + " and I'm " + age + "years old";
 
+const value = `Hey it's me ${name} and I'm ${age} years old ` // backticks (``) instead of ("") and ${} instead of concatenation
 
-console.log(textLowerCase); // text-transform: uppercase;
-console.log(textUpperCase); // text-transform: lowercase;
-console.log(text.charAt(2)); // return the value at specified index
-console.log(text.charAt(12).toUpperCase()); 
-console.log(text.charAt(text.length-1)); 
-console.log(text.indexOf('J')); // returns index of passed value
-console.log(text.trim()); // trims whitespace from start and end of string
-console.log(text.trim().startsWith('Peter')); // returns true or false if it starts with passed value
-console.log(text.includes('ete')); // returns true or false if string has specified value within
-console.log(text.slice(8, 12)); // index slicing (starts at 0)
-console.log(text.slice(-4)); // index slicing reversed
+console.log(value);
