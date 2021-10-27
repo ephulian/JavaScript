8bb2642a92729f244d4354f19d6e8276be8ccc14
Kiril Komitski <k.komitski94@gmail.com>
2021-10-27 16:43:10 +0100

    Array methods

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const name = 'john';
-const age = 25;
-const sentence = "Hey it's me " + name + " and I'm " + age + "years old";
+let names = ['john', 'peter', 'bob', 'barry', 'james', 'glen', 4, 2, 1];
+let lastNames = ['peper', 'onion', 'phone', 'clamp'];
 
-const value = `Hey it's me ${name} and I'm ${age} years old ` // backticks (``) instead of ("") and ${} instead of concatenation
+console.log(names.length); // returns length of array
+console.log(names[4]); // returns value at specific index (starting at 0)
+console.log(names[names.length - 6]); // returns value at specific index (reversed)
 
-console.log(value);
+console.log(names.concat(lastNames)); // appends specified array
+console.log(names.reverse()); // reverses array
+
+console.log(names.unshift('suzy')); // adds to beggining of array
+console.log(names.shift()); // removes first item in array
+
+console.log(names.push('anna')); // appends to end of array
+console.log(names.pop()); // removes from end of array
+
+console.log(names.splice(0, 3)); // mutates original array!!
+
+console.log(names);
