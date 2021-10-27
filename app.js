5960c9f3f503c8f1ab85a9ec27fbb4d01ad218db
Kiril Komitski <k.komitski94@gmail.com>
2021-10-27 13:52:00 +0100

    switch()/case/default

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
+const dice = 1;
 
-const name ='peter';
-const age = 24;
+// if (dice === 1){
+//     console.log('you got one');
+// }
+// else if (dice === 2){
+//     console.log('you got two');
+// }
+// else {
+//     console.log("you didn't roll the dice");
+// }
 
-if(name === 'peter' && age === 24) {
-    console.log('hello there user');
-} else {
-    console.log('wrong values');
+switch(dice){
+    case 1:
+        console.log('you got one');
+        break;
+    case 2:
+        console.log('you got two');
+        break;
+    case 3:
+        console.log('you got three');
+        break;
+    default:
+        console.log('you did not roll');
 }
 
 // || is OR and && is AND
