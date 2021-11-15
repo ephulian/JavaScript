2388b8f8d544e12c588ff9fe17ad50f8e43516d8
Kiril Komitski <k.komitski94@gmail.com>
2021-11-15 17:57:23 +0000

    reduce method

--- app2.js
+++ app2.js
@@ -1,17 +1,25 @@
 const people = [
-    {name: 'bob', age: 20, position: 'dev', id: 1},
-    {name: 'paul', age: 25, position: 'des', id: 2},
-    {name: 'george', age: 30, position: 'den', id: 3},
-    {name: 'bobby', age: 35, position: 'del', id: 4},
-    {name: 'prin', age: 40, position: 'deg', id: 5}    
+    {name: 'bob', age: 20, position: 'dev', id: 1, salary: 200},
+    {name: 'paul', age: 25, position: 'des', id: 2, salary: 250},
+    {name: 'george', age: 30, position: 'den', id: 3, salary: 300},
+    {name: 'bobby', age: 35, position: 'del', id: 4, salary: 350},
+    {name: 'prin', age: 40, position: 'deg', id: 5, salary: 400}    
 ];
 
-const person = people.find((blit) => blit.id === 3)
+const bob = people.reduce((acc, currItem) => {
+    console.log(acc);
+    console.log(currItem);
+    return acc
+}, 0)
 
-console.log(person);
+const items = [
+    {name:'rice', price:5},
+    {name:'book', price:20},
+    {name:'chicken', price:10},
+    {name:'pork', price:100},
+];
 
-const youngPerson = people.find((cunt) => cunt.age === 40)
-console.log(youngPerson);
+const reducer = (total, item) => total + item
 
-// filter returns an array full of objects
-// find returns an object (and always the first match)
+let total = items.reduce(reducer, 0)
+console.log(total);
