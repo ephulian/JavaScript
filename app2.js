659dca97e2d81c241451aaf8d7073e97ad703149
Kiril Komitski <k.komitski94@gmail.com>
2021-11-15 17:05:13 +0000

    map

--- app2.js
+++ app2.js
@@ -1,23 +1,49 @@
-// function greetMorning(name){
-//     const myName = 'Kiril';
-//     console.log(`Good morning ${name}, my name is ${myName}`);
-// }
+const people = [
+    {name:'bob', age:20, position:'dev'},
+    {name:'peter', age:25, position:'design'},
+    {name:'josh', age:30, position:'boss'}
+]
 
-// function greetAfternoon(name){
-//     const myName = 'Kiril';
-//     console.log(`Good afternoon ${name}, my name is ${myName}`);
-// }
 
-const currentTime = 12
 
-const morning = (name) => `Good morning ${name}`;
-const afternoon = (name) => `Good afternoon ${name}`;
-const evening = (name) => `Good evening ${name}`;
+const ages = people.map(function(item){
+    return item.name;
+});
 
-function greet(name, callback){
-    const myName = 'Kiril';
-    console.log(`${callback(name)}, my name is ${myName}!`);
-    callback();
-}
+console.log(ages);
 
-greet('Bobo', evening)
+const newPeople = people.map((person) => {
+    return {
+        firstName: person.name.toUpperCase(),
+        oldAge: person.age + 20
+    }
+})
+
+const names = people.map((person) => {
+    return `<h1>${person.name}</h1>`
+})
+
+document.body.innerHTML = names.join('')
+
+console.log(...newPeople);
+console.log(names);
+
+
+// const cabinets = [
+//     {name:'unit1', type:'tall', color:'blue', height:175, doors:2},
+//     {name:'unit1', type:'bottom', color:'yellow', height:112, doors:4},
+//     {name:'unit1', type:'tall', color:'yellow', height:112, doors:4},
+//     {name:'unit1', type:'wall', color:'yellow', height:112, doors:4},
+//     {name:'unit1', type:'under-stair', color:'yellow', height:112, doors:4},
+//     {name:'unit1', type:'top', color:'orange', height:50, doors:6}
+// ]
+
+// // const yellowUnits = cabinets.map(function(unit){
+// //         return unit.color
+// // });
+
+// const yellowUnits = cabinets.map((unit) => {
+//     // return unit.color(
+// })
+
+// console.log(yellowUnits); 
