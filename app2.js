69cb6b1ff22480b39ab72328602a8635939e1983
Kiril Komitski <k.komitski94@gmail.com>
2021-11-29 15:54:06 +0000

    Math Object

--- app2.js
+++ app2.js
@@ -1,29 +1,15 @@
-const people = [
-    {name: 'bob', age: 20, position: 'dev', id: 1, salary: 200},
-    {name: 'paul', age: 25, position: 'des', id: 2, salary: 250},
-    {name: 'george', age: 30, position: 'den', id: 3, salary: 300},
-    {name: 'bobby', age: 35, position: 'del', id: 4, salary: 350},
-    {name: 'prin', age: 40, position: 'deg', id: 5, salary: 400}    
-];
+// Math
+// Standard built-in objects - always available
+// Math.random returns a value between 0 and 1 (but never 1) 
+// multiply to move the decimal point and floor or ceil to round up or down
 
-const bob = people.reduce((acc, currItem) => {
-    console.log(acc)
-    console.log(currItem)
-    return acc
-}, 0)
 
-const items = [
-    {name:'rice', price:5},
-    {name:'book', price:20},
-    {name:'chicken', price:10},
-    {name:'pork', price:100},
-]
+const number = 4.56789;
 
-const reducer = (total, item) => total + item
+const result = Math.ceil(number);
 
-let total = items.reduce(reducer, 0)
-console.log(total)
+console.log(result);
 
-// const var = object.reduce(function(acc, curr){}, start)
-// acc = total of all calculations
-// curr = current iteration
+const randInt = Math.floor(Math.random() * 10 + 1);
+
+console.log(randInt);
