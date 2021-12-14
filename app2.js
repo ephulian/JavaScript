5b8c25f4f1ba8fc9c8c6b96aa3f1cc70a0b5063a
Kiril Komitski <k.komitski94@gmail.com>
2021-12-14 08:40:33 +0000

    insertBefore

--- app2.js
+++ app2.js
@@ -3,10 +3,9 @@ const second = document.getElementById('second');
 const third = document.getElementById('third');
 const result = document.getElementById('result');
 
-// create empty element
-const bodyDiv = document.createElement('div');
+const newListElement = document.createElement('li');
+const blueberry = document.createTextNode('blueberry')
 
-// create text node
-const text = document.createTextNode('simple body div');
-bodyDiv.appendChild(text)  // append text to div
-document.body.appendChild(bodyDiv) // append div to document > body
+// insertBefore (first value is item inserted, second value is before which item)
+result.insertBefore((newListElement.appendChild(blueberry)), second)
+document.body.appendChild(result)
