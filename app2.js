ecda1311806c7883067eb61b626dab922112e717
Kiril Komitski <k.komitski94@gmail.com>
2021-12-15 14:18:06 +0000

    addEventListener('click', () => {})

--- app2.js
+++ app2.js
@@ -1,14 +1,23 @@
 const first = document.getElementById('first');
 const second = document.getElementById('second');
 const third = document.getElementById('third');
+
 const result = document.getElementById('result');
 
-// returns the text content
-console.log(third.textContent); 
+const button = document.querySelector('.btn');
 
-// returns the whole html structure
-console.log(result.innerHTML); 
 
-const ul = document.createElement('ul'); // create new element
-ul.innerHTML = `<li>new list item</li>` // populate with HTML
-document.body.appendChild(result.appendChild(ul)) // append to DOM
+button.addEventListener('click', () => {
+    let currentColor = button.getAttribute('class').split(' ')[1] //?
+    console.log(currentColor); //?
+    switch(currentColor){
+        case 'red':
+            button.removeAttribute('red')
+            button.setAttribute('class', 'blue')
+            break
+        case 'blue':
+            button.removeAttribute('blue')
+            button.setAttribute('class', 'red')
+            break
+    }
+})
