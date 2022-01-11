33134c988adef8dbfcb1dfc31b8e0bfbc7935f7b
Kiril Komitski <k.komitski94@gmail.com>
2022-01-11 17:23:01 +0000

    bind method

--- app2.js
+++ app2.js
@@ -17,14 +17,19 @@ function greet(city, country){
 // first argument for .call and .apply method is - the value to be represented by this keyword
 // apply is the same but it take the arguments as an array
 
-// example Prototype.apply(this, [arg1, arg2, arg3])
+// example Function.apply(this, [arg1, arg2, arg3])
 greet.apply(susan, ['san-diego', 'us'])
 greet.apply(john)
 greet.apply({name: 'peterosk', age: 42})
 
 // first argument is 'this' pointer and all other are list of arguments 
 
-// example Prototype.apply(this, arg1, arg2, arg3)
+// example Function.apply(this, arg1, arg2, arg3)
 greet.call(susan, 'san-diego', 'us')
 greet.call(john)
 greet.call({name: 'peterosk', age: 42})
+
+// with bind you assign to call later
+// example Function.bind(this, arg1, arg2)
+const susanGreet = greet.bind(susan, 'toronto', 'france')
+susanGreet()
