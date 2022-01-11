bf674dc00fcef7f5cfaae6e53f6d43ad409ca7b7
Kiril Komitski <k.komitski94@gmail.com>
2022-01-11 16:03:33 +0000

    Prototypes

--- app2.js
+++ app2.js
@@ -1,22 +1,40 @@
-// Constructor function
-function Person(firstName, lastName){
-    this.firstName = firstName
-    this.lastName = lastName
-    this.fullName = function (){
-        console.log(`full name: ${this.firstName} ${this.lastName}`);
+// Everything is derived of the prototype of Object
+// Example Object >> Function >> () => {}
+// Or Object >> Array >> myArray
+
+function Account(name, initialBalance){
+    this.name = name;
+    this.initialBalance = initialBalance;
+    this.balance = 0
+    this.checkBalance = function(){
+        console.log(`${this.name}'s balance is ${this.balance}`);
     }
 }
 
-// Everything in JS has a constructor property that returns the function that created it
+// Create a new constructor property
+Account.prototype.bank = 'HSBC'
 
-const james = new Person('james', 'malcovich');
-console.log(james.constructor);
+// Its best to use prototype to add methods to objects (to avoid creation on each instance)
+Account.prototype.deposit = function(amount){
+    this.balance += amount;
+}
+
+const jinny = new Account('jinny', 10);
+jinny.deposit(15)
+jinny.checkBalance()
+
+const timmy = new Account('timmy', 24848);
+timmy.deposit(500)
+timmy.deposit(500)
+timmy.deposit(500)
+timmy.checkBalance()
+
+
+console.log(Account.prototype);
+console.log(timmy.bank);
 
 const obj = {};
-console.log(obj.constructor);
+console.log(obj);
 
 const list = [];
-console.log(list.constructor);
-
-const sayHi = () => {};
-console.log(sayHi.constructor);
+console.log(list.constructor.constructor);
