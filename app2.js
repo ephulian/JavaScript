285f8199f6cdf2ebc192d9ab2a86d9888e4b567b
Kiril Komitski <k.komitski94@gmail.com>
2022-01-11 13:54:22 +0000

    this keyword basics

--- app2.js
+++ app2.js
@@ -1,37 +1,23 @@
-const age2 = 60
-const age3 = 70
+// this points to the left of dot
 
-let random = 'random-value'
-random = 'age'
-
-// Nesting objects
-const person = {
-    name: 'john',
-    age: 25,
-    age2: age2, // setting property value to variable value
-    age3, // ES6 allows auto set if property name == var name
-    married: true,
-    siblings: ['anna', 'james', 'peter'],
-    greet: (name) => {
-        console.log(`hello, my name is ${name}`);
-    },
-    sayHello(name){
-        console.log(`hello, my name is ${name}`);
-    },
-    job:{
-        title:'developer',
-        company: {
-            name:'perspective concepts',
-            address: '123 main'
-        }
-    },
-    'random-value':'random' // string key can be accessed with bracket notation []
+const john = {
+    firstName: 'Kiril',
+    lastName: 'Komitski',
+    fullName: function(){
+        // console.log(`My full name is Kiril Komitski`);
+        console.log(`My full name is ${this.firstName} ${this.lastName}`);
+    }
 }
 
-// Use dot notation to drop down nested objects
-console.log(person.job.title); //? developer
-console.log(person['name']); // also works
+// this does NOT work with arrow syntax
+const bob = {
+    firstName: 'Bob',
+    lastName: 'Builder',
+    fullName: ()=> {
+        // console.log(`My full name is Bob Builder`);
+        console.log(`My full name is ${this.firstName} ${this.lastName}`);
+    }
+}
 
-
-console.log(person["random-value"]); //? random
-console.log(person[random]); //? can use a var to dynamically return property values
+john.fullName();
+// bob.fullName();
