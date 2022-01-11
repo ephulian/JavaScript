968a4121dfd1ba6187fca325a867d1c608a130fa
Kiril Komitski <k.komitski94@gmail.com>
2022-01-11 15:02:22 +0000

    constructor property/method

--- app2.js
+++ app2.js
@@ -1,17 +1,3 @@
-// Factory function
-function createPerson(firstName, lastName){
-    return {
-        firstName,
-        lastName,
-        fullName: function(){
-            console.log(`my full name is ${this.firstName} ${this.lastName}`);
-        }
-    }
-}
-
-const peter = createPerson('peter', 'ilfried');
-peter.fullName()
-
 // Constructor function
 function Person(firstName, lastName){
     this.firstName = firstName
@@ -21,5 +7,16 @@ function Person(firstName, lastName){
     }
 }
 
-const james = new Person('james', 'malcovich'); // new creates new instance of object and points to it
-console.log(james.firstName);
+// Everything in JS has a constructor property that returns the function that created it
+
+const james = new Person('james', 'malcovich');
+console.log(james.constructor);
+
+const obj = {};
+console.log(obj.constructor);
+
+const list = [];
+console.log(list.constructor);
+
+const sayHi = () => {};
+console.log(sayHi.constructor);
