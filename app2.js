47dcd1ce813c45a47b50b2df42cd76b8cf8b19b7
Kiril Komitski <k.komitski94@gmail.com>
2022-01-13 11:43:10 +0000

    Promise real world example (photo load)

--- app2.js
+++ app2.js
@@ -2,35 +2,30 @@ const one = document.querySelector('.one');
 const two = document.querySelector('.two');
 const three = document.querySelector('.three');
 
+const container = document.querySelector('.img-container');
+const url = 'https://source.unsplash.com/random';
+
 const btn = document.querySelector('.btn');
 const btn2 = document.querySelector('.btn-2');
 
-// Set-up a promise object with resolve and reject options
-// Inside Promise object create some functionality
-// Depending on the outcome of said functionality set an output value
-// resolve for promise functionality fullfilled
-// reject for promise functionality unfullfilled
-
-const promise = new Promise((resolve, reject) => {
-	let value = false; // functionality
-	if (value) {
-		resolve([1, 2, 3]); // returns for .then
-	} else {
-		reject(() => {
-			// returns for .catch
-			console.log('this is called now');
-		});
-	}
+btn.addEventListener('click', () => {
+	console.log(loadImage(url));
+	loadImage(url)
+		.then((data) => {
+			container.appendChild(data);
+		})
+		.catch(error);
 });
 
-// Run the promise functionality
-// If 'resolved' run .then
-// If 'rejected' run .catch
-
-promise
-	.then((result) => {
-		console.log(result);
-	})
-	.catch((result) => {
-		console.log(result);
+function loadImage(url) {
+	return new Promise((resolve, reject) => {
+		let img = new Image();
+		img.addEventListener('load', () => {
+			return resolve(img);
+		});
+		img.addEventListener('error', () => {
+			return reject(new Error(`Failed to load image from the ${url}`));
+		});
+		img.src = url;
 	});
+}
