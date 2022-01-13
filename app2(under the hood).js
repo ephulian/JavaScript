3a68089a6c31212d8f230ada52e6794bec0b3019
Kiril Komitski <k.komitski94@gmail.com>
2022-01-13 10:29:10 +0000

    Nested Callbacks (callback hell)

--- app2(under the hood).js
+++ app2(under the hood).js
@@ -2,88 +2,91 @@
 
 // class Account
 // ===============================================================================================================
-function Account(name, initialBalance){
-    this.name = name;
-    this.initialBalance = initialBalance;
-    this.balance = 0
+function Account(name, initialBalance) {
+	this.name = name;
+	this.initialBalance = initialBalance;
+	this.balance = 0;
 }
 
 // Create a new constructor property
-Account.prototype.bank = 'HSBC'
+Account.prototype.bank = 'HSBC';
 
 // Its best to use prototype to add methods to objects (to avoid creation on each instance)
-Account.prototype.deposit = function(amount){
-    this.balance += amount;
-}
+Account.prototype.deposit = function (amount) {
+	this.balance += amount;
+};
 
-Account.prototype.checkBalance = function(){
-    console.log(`${this.name}'s balance is ${this.balance}`);
-} 
+Account.prototype.checkBalance = function () {
+	console.log(`${this.name}'s balance is ${this.balance}`);
+};
 // ===============================================================================================================
 
 // ES6 Syntax ===============================================================================================================
-class AccountES6 { // always uppercase for class names
-    constructor(name, initialBalance){
-        // all values here are unique to the specific instance
-        // they need to be declared
-        this.name = name; // parameters (to be args)
-        this.balance = initialBalance; // parameters (to be args)
-    }
-    // all values here are applied to all instances
-    bank = 'HSBC' // !!! dont need .this keyword !!!
-    deposit(amount){ // dont need function declaration
-        this.balance += amount; // to add methods
-    }
-    checkBalance(){
-        console.log(`${this.name}'s balance is ${this.balance}`);
-    }
-    any(){}
+class AccountES6 {
+	// always uppercase for class names
+	constructor(name, initialBalance) {
+		// all values here are unique to the specific instance
+		// they need to be declared
+		this.name = name; // parameters (to be args)
+		this.balance = initialBalance; // parameters (to be args)
+	}
+	// all values here are applied to all instances
+	bank = 'HSBC'; // !!! dont need .this keyword !!!
+	deposit(amount) {
+		// dont need function declaration
+		this.balance += amount; // to add methods
+	}
+	checkBalance() {
+		console.log(`${this.name}'s balance is ${this.balance}`);
+	}
+	any() {}
 }
 
 // ============================================================================
 
-function getElement (selection){
-    const element = document.querySelector(selection);
-    if(element){
-        return element
-    }
-    throw new Error(`Please check "${selection}" selector, no such element exist in document.`)
+function getElement(selection) {
+	const element = document.querySelector(selection);
+	if (element) {
+		return element;
+	}
+	throw new Error(
+		`Please check "${selection}" selector, no such element exist in document.`
+	);
 }
 
-function Counter (element, value){
-    this.counter = element;
-    this.value = value;
-    this.resetBtn = element.querySelector('.resset')
-    this.increaseBtn = element.querySelector('.increase')
-    this.decreaseBtn = element.querySelector('.decrease')
-    this.valueDOM = element.querySelector('.value')
-    this.valueDOM.innerHTML = this.value
+function Counter(element, value) {
+	this.counter = element;
+	this.value = value;
+	this.resetBtn = element.querySelector('.resset');
+	this.increaseBtn = element.querySelector('.increase');
+	this.decreaseBtn = element.querySelector('.decrease');
+	this.valueDOM = element.querySelector('.value');
+	this.valueDOM.innerHTML = this.value;
 
-    // bind this to all functions
-    this.increase = this.increase.bind(this)
-    this.decrease = this.decrease.bind(this)
-    this.reset = this.reset.bind(this)
+	// bind this to all functions
+	this.increase = this.increase.bind(this);
+	this.decrease = this.decrease.bind(this);
+	this.reset = this.reset.bind(this);
 
-    this.increaseBtn.addEventListener('click', this.increase)
-    this.decreaseBtn.addEventListener('click', this.decrease)
-    this.resetBtn.addEventListener('click', this.reset)
-    
+	this.increaseBtn.addEventListener('click', this.increase);
+	this.decreaseBtn.addEventListener('click', this.decrease);
+	this.resetBtn.addEventListener('click', this.reset);
 }
 
-Counter.prototype.increase = function(value){
-    this.value++
-    this.valueDOM.innerHTML = this.value
-}
+Counter.prototype.increase = function (value) {
+	this.value++;
+	this.valueDOM.innerHTML = this.value;
+};
 
-Counter.prototype.decrease = function(value){
-    this.value--
-    this.valueDOM.innerHTML = this.value
-}
+Counter.prototype.decrease = function (value) {
+	this.value--;
+	this.valueDOM.innerHTML = this.value;
+};
 
-Counter.prototype.reset = function(){
-    this.value = 0
-    this.valueDOM.innerHTML = this.value
-}
+Counter.prototype.reset = function () {
+	this.value = 0;
+	this.valueDOM.innerHTML = this.value;
+};
 
-const firstCounter = new Counter(getElement('.first-counter'), 100)
-const secondCounter = new Counter(getElement('.second-counter'), 300)
+const firstCounter = new Counter(getElement('.first-counter'), 100);
+const secondCounter = new Counter(getElement('.second-counter'), 300);
--- app2.js
+++ app2.js
@@ -1,10 +1,27 @@
-boilWater(5000);
-console.log('chop onion');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
 
-function boilWater(time) {
-	console.log('boiling...');
+const btn = document.querySelector('.btn');
+const btn2 = document.querySelector('.btn-2');
+
+btn.addEventListener('click', () => {
+	// console.log('you clicked BTN');
 	setTimeout(() => {
-		// setTimeout is a BROWSER function, not JS
-		console.log('boiled');
-	}, time);
-}
+		one.style.color = 'red';
+		setTimeout(() => {
+			two.style.color = 'green';
+			setTimeout(() => {
+				three.style.color = 'blue';
+			}, 1000);
+		}, 1000);
+	}, 1000);
+});
+
+btn2.addEventListener('click', () => {
+	one.style.color = 'black';
+	two.style.color = 'black';
+	three.style.color = 'black';
+});
+
+console.log(`im second`);
