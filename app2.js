e424e4440a8ff7c5cada47150c344fd49ae4686d
Kiril Komitski <k.komitski94@gmail.com>
2022-01-18 09:17:26 +0000

    Arrow vs Reg .this scope

--- app2.js
+++ app2.js
@@ -1,71 +1,25 @@
-// Function declaration - HOISTED
-function makeBread(qty) {
-	return 'bread '.repeat(qty);
-}
-
-makeBread(6);
-
-// Function expression - NOT HOISTED
-const makeCake = function (qty) {
-	return 'cake '.repeat(qty);
+// Arrow function refers to parent score (bob object scope here)
+const bob = {
+	name: 'bob',
+	age: 23,
+	sayName: function () {
+		setTimeout(() => {
+			console.log(`My name is ${this.name} and I am ${this.age} years old`);
+		}, 1000);
+	},
 };
 
-makeCake(6);
+// Regular function expression refers to current scope (sayName function here)
+const mary = {
+	name: 'mary',
+	age: 26,
+	sayName: function () {
+		const self = this; // can bind to variable and drop in next scope
+		setTimeout(function () {
+			console.log(`My name is ${self.name} and I am ${self.age} years old`);
+		}, 1000);
+	},
+};
 
-// Arrow function expression
-const makeKefir = (qty) => 'kefir '.repeat(qty);
-
-makeKefir(6);
-
-// IIFE == 'Immediately invoked function expressions'
-(() => {
-	return 'bread '.repeat(6);
-})();
-
-((a, b) => {
-	return a + b;
-})(2, 2);
-
-// Closures and higher order functions
-function outer() {
-	const private = 'secret';
-	console.log('this is outside');
-	function inner() {
-		console.log('this is inside');
-		console.log(`the variable is ${private}`);
-	}
-	return inner;
-}
-
-const innerFunct = outer(); // save the return statement for outside to a variable
-innerFunct(); // invoke the return statement for outer
-
-outer()(); // first one invokes 'outer' which returns 'inner' that is invoked with 2nd ()
-
-// Return as objects to create methods
-function newAccount(name, innitailBalance) {
-	let balance = innitailBalance; // not referred to the return statement object - hence not accessable from outside
-
-	function showBalance() {
-		console.log(`Hey! My name is ${name} and I have ${balance} dollars`);
-	}
-	function withdraw(amount) {
-		balance -= amount;
-		showBalance();
-	}
-	function deposit(amount) {
-		balance += amount;
-		showBalance();
-	}
-	return {
-		showBalance: showBalance,
-		withdraw: withdraw,
-		deposit: deposit,
-	};
-}
-
-const john = newAccount('john', 500);
-const peter = newAccount('peter', 250);
-
-john.withdraw(200);
-peter.showBalance();
+bob.sayName();
+mary.sayName();
