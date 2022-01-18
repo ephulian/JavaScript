94e15f429d9e854de645f8742facffda4c93ad36
Kiril Komitski <k.komitski94@gmail.com>
2022-01-18 09:19:40 +0000

    Array Destructuring

--- app2.js
+++ app2.js
@@ -1,25 +1,9 @@
-// Arrow function refers to parent score (bob object scope here)
-const bob = {
-	name: 'bob',
-	age: 23,
-	sayName: function () {
-		setTimeout(() => {
-			console.log(`My name is ${this.name} and I am ${this.age} years old`);
-		}, 1000);
-	},
-};
+// Array destructuring
 
-// Regular function expression refers to current scope (sayName function here)
-const mary = {
-	name: 'mary',
-	age: 26,
-	sayName: function () {
-		const self = this; // can bind to variable and drop in next scope
-		setTimeout(function () {
-			console.log(`My name is ${self.name} and I am ${self.age} years old`);
-		}, 1000);
-	},
-};
+const fruits = ['orange', 'apple', 'lemon', 'banana'];
 
-bob.sayName();
-mary.sayName();
+const [orange, apple, lemon] = fruits;
+
+console.log(orange);
+console.log(apple);
+console.log(lemon);
